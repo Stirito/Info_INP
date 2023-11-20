1e655aaabee890dcf3dfecb72b36a24036b83604
ByStirith <benoit.ferrere11@gmail.com>
2023-11-20 07:32:27 +0400

    f

--- revision_DS.py
+++ revision_DS.py
@@ -1,7 +1,7 @@
 from Pilescopy import *
 
 
-P,Q = [9,4,2,17,6,9], [17,2,3,19,4,6,20]
+P,Q = [9,4,2,7,6,9], [17,2,3,19,4,6,20]
 
 def Max(P:list):
     L = Creer_Pile()
@@ -17,8 +17,11 @@ def Max(P:list):
             b = a
         a = Depiler(P)
         Empiler(L,a)
-  
-    return abs(Longueur(L)-trouve_indice_valeur(L,b))+1
+    G = Creer_Pile()
+    
+    while L != []:
+        Empiler(G,Depiler(L))
+    return trouve_indice_valeur(G,b)
 
 def trouve_indice_valeur(P,valeur):
     j = 1
@@ -28,5 +31,35 @@ def trouve_indice_valeur(P,valeur):
             break
         j+=1
     return j
-print(Max(Q),Max(P))
 
+
+def Somme(P:list):
+    L = Creer_Pile()
+    somme = 0
+    if Pile_Vide(P):
+        raise Exception("Pile vide pas de somme")
+    else:
+        while P != []:
+            a = Depiler(P)
+            somme+=a
+            Empiler(L,a)
+        while L != []:
+            Empiler(P,Depiler(L))
+    return P,somme
+
+def Renverser(P:list,j:int):
+    L = Creer_Pile()
+    D = Creer_Pile()
+    for i in range(j):
+        a = Depiler(P)
+        Empiler(L,a)
+    
+  
+    while L != []:
+        Empiler(D,Depiler(L))
+    
+    while D != []:
+        Empiler(P,Depiler(D))
+    return P
+
+print(Renverser(P,3))
