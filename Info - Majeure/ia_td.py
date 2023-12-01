021c37e21649e2eb8d8c01387d7e9af8adf7eefe
ByStirith <benoit.ferrere11@gmail.com>
2023-12-01 19:24:52 +0400

    Update ia_td.py

--- Info - Majeure/ia_td.py
+++ Info - Majeure/ia_td.py
@@ -8,3 +8,32 @@ class Joueur:
         
     def getInfo(self):
         return (self.nom,self.nature)
+    
+
+
+class Jeu:
+    def __init__(self,nom="NIM",joueurs=[],allumettes=15,vainqueur=None,note =0):
+        self.nom = nom
+        self.joueurs = joueurs
+        self.allumettes = allumettes
+        self.vainqueur = vainqueur
+        self.note = note
+    def getInfo(self):
+        return (self.nom,self.joueurs,self.allumettes,self.vainqueur,self.note)
+    def Afficher(self):
+        for i in range(self.allumettes):
+            print("|",end=" ")
+    def getJoueur(self):
+        return self.joueurs[0]
+    def getPartieFinie(self):
+        return self.allumettes == 0 or self.allumettes == 1
+    def setVainqueur(self,joueur):
+        self.vainqueur = joueur
+    def Passer(self):
+        self.joueurs = [self.joueurs[1],self.joueurs[0]]
+    def getCanPlay(self):
+        return self.allumettes >=2
+    def getSituSuivantes():
+        liste_situation = []
+        return liste_situation
+    
